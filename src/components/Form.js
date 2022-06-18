be95fcf1e6f7a0d3770e979211078e51b4c38dc5
Dima <doberman.info@gmail.com>
2022-06-18 18:00:11 +0300

    done

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,9 +1,23 @@
-import React from "react";
+
+import React, {useState} from "react";
+
+
+
 
 function Form(props) {
+    const [name, setName] = useState('');
+
+    function handleChange(e) {
+        setName(e.target.value);
+    }
+
     function handleSubmit(e) {
-        e.preventDefault();
-        alert('Hello,world!');
+        if (!name) return;
+        else { e.preventDefault();
+            props.addTask(name);
+            setName("");}
+       
+    
     }
     return ( <
         form onSubmit={handleSubmit}>
@@ -20,7 +34,9 @@ function Form(props) {
         id = "new-todo-input"
         className = "input input__lg"
         name = "text"
-        autoComplete = "off" /
+        autoComplete = "off"
+        value={name} 
+        onChange={handleChange}/
         >
         <
         button type = "submit"
--- src/components/Todo.js
+++ src/components/Todo.js
@@ -9,6 +9,7 @@ export default function Todo(props) {
         input id = {props.id}
         type = "checkbox"
         defaultChecked = { props.completed }
+        onChange= {() => props.toggleTaskCompleted(props.id)}
         /> <
         label className = "todo-label"
         htmlFor = {props.id} > { props.name } <
@@ -21,7 +22,8 @@ export default function Todo(props) {
         Edit < span className = "visually-hidden" > { props.name } < /span> < /
         button > <
         button type = "button"
-        className = "btn btn__danger" >
+        className = "btn btn__danger" 
+        onClick={() => props.deleteTask(props.id)}>
         Delete < span className = "visually-hidden" > { props.name } < /span> < /
         button > <
         /div> < /
