f572aa63366e76cade1a7831ba327149ad6b0093
Dima <doberman.info@gmail.com>
2022-06-19 18:15:12 +0300

    focus

--- src/App.js
+++ src/App.js
@@ -1,11 +1,31 @@
-import React, {useState} from "react";
+import React, {useState, useRef, useEffect} from "react";
 import Form from "./components/Form";
 import FilterButton from "./components/FilterButton";
 import Todo from "./components/Todo";
 import {nanoid} from "nanoid";
+import usePrevious from "./components/UsePrevious";
+
+const FILTER_MAP = {
+    All: () => true,
+    Active: task => !task.completed,
+    Completed: task => task.completed
+};
+
+const FILTER_NAMES = Object.keys(FILTER_MAP);
 
 function App(props) {
     const [tasks, setTasks] = useState(props.tasks);
+    const [filter, setFilter] = useState('All');
+
+    function editTask(id, newName) {
+        const editedTaskList = tasks.map(task => {
+            if (id === task.id) {
+                return {...task, name: newName}
+            }
+            return task;
+        });
+        setTasks(editedTaskList);
+    }
 
     function addTask(name) {
 
@@ -31,7 +51,9 @@ function App(props) {
         setTasks(remainingTasks);
     }
 
-  const taskList = tasks.map(task => (
+  const taskList = tasks
+  .filter(FILTER_MAP[filter])
+  .map(task => (
     <Todo
         id={task.id}
         name={task.name}
@@ -39,21 +61,29 @@ function App(props) {
         key={task.id}
         toggleTaskCompleted={toggleTaskCompleted}
         deleteTask={deleteTask}
+        editTask={editTask}
       />
     )
   );
+  const filterList = FILTER_NAMES.map(name => (<FilterButton 
+    key={name} name={name} isPressed={name === filter} setFilter={setFilter}/>));
   const tasksNoun = taskList.length !== 1 ? 'tasks' : 'task';
   const headingText = `${taskList.length} ${tasksNoun} remaining`;
+  const ListHeadingRef = useRef(null);
+  const prevTaskLength = usePrevious(tasks.length);
+  useEffect(() => {
+    if (tasks.length - prevTaskLength === -1) {
+        ListHeadingRef.current.focus();
+    }
+  }, [tasks.length, prevTaskLength]);
   return (
     <div className="todoapp stack-large">
       <h1>TodoMatic</h1>
       <Form addTask={addTask}/>
       <div className="filters btn-group stack-exception">
-        <FilterButton />
-        <FilterButton />
-        <FilterButton />
+      {filterList}
       </div>
-      <h2 id="list-heading">{headingText}</h2>
+      <h2 id="list-heading" tabIndex="-1" ref={ListHeadingRef}>{headingText}</h2>
       <ul
        // role="list"
         className="todo-list stack-large stack-exception"
